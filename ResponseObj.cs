9abc2cbe4f8c369bcdef44f81efc492446ab82b8
APIVerve Team <hello@apiverve.com>
2024-05-14 15:43:23 -0500

    Bug fixes and Improvements

--- ResponseObj.cs
+++ ResponseObj.cs
@@ -1,21 +1,35 @@
 using System;
 using System.Collections.Generic;
-using System.Text;
+using Newtonsoft.Json;
 
 namespace APIVerve
 {
-    using Newtonsoft.Json;
+public class data
+{
+    [JsonProperty("originalWords")]
+    public int originalWords { get; set; }
+
+    [JsonProperty("summaryWords")]
+    public int summaryWords { get; set; }
+
+    [JsonProperty("percentDifference")]
+    public double percentDifference { get; set; }
+
+    [JsonProperty("summary")]
+    public string summary { get; set; }
+
+}
 
 public class ResponseObj
 {
     [JsonProperty("status")]
-    public string Status { get; set; }
+    public string status { get; set; }
 
     [JsonProperty("error")]
-    public string Error { get; set; }
+    public object error { get; set; }
 
     [JsonProperty("data")]
-    public object Data { get; set; }
+    public data data { get; set; }
 
 }
 
