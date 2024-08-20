e97c4fbb6bdcad46bae94d026f0cfc8959716e8b
APIVerve Team <hello@apiverve.com>
2024-08-20 11:10:46 -0500

    Bug Fixes

---
+++ TextSummarizerQueryOptions.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class TextSummarizerQueryOptions {
+public string text { get; set; }
+public int sentences { get; set; }
+}
+
+}
